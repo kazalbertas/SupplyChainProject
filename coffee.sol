071fb4c30748a399690a987a511493b02ce09c74
Albertas <albertaskaz4@gmail.com>
2019-10-22 13:56:06 +0200

    Updated payments code (TODO), Added working transfer location.

--- coffee.sol
+++ coffee.sol
@@ -1,16 +1,52 @@
 pragma solidity ^0.5.2;
 pragma experimental ABIEncoderV2;
 
+contract Payment
+{
+    address payable public from;
+    address payable public to;
+    uint public amount;
+    bool public approved = false;
+    
+    constructor (address payable _to, address payable _from, uint _amount) public
+    {
+        to = _to;
+        from = _from;
+        amount = _amount;
+    }
+    
+    function approve(uint sum) public payable {
+        if (!approved) {
+            if (msg.sender == from) {
+                if (sum >= amount){
+                    // to.send(amount);
+                    approved = true;
+                }
+            }
+        }
+    }
+}
+
+
 contract Coffee
 {
     
-    struct TransferEntry{
-        address from;
-        address to;
+    
+    string public batchId;
+    string[] public bagIds;
+    
+    int public minimumPricePerBag = 1;
+    int public minimumPrice;
+    
+    constructor (string memory _batchId, string[] memory _bagIds) public
+    {
+        batchId = _batchId;
+        owner = msg.sender;
+        bagIds = _bagIds;
+        locationOwner = msg.sender;
     }
     
     
-    string public batchId;
     
     // Approve section
     address private approver = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
@@ -25,11 +61,16 @@ contract Coffee
     }
     ////////////////////////////////////////////////////
     
-    
-    
+    // Owner section
     
     address public owner;
-    TransferEntry[] transfers;
+    address public futureOwner;
+    
+    struct TransferOwnershipEntry{
+        address from;
+        address to;
+        address payment;
+    }   
     
     event TransferCompleted(
         address from,
@@ -37,11 +78,77 @@ contract Coffee
         string batchId
     );
     
-    constructor (string memory _batchId) public
-    {
-        batchId = _batchId;
-        owner = msg.sender;
+    
+    TransferOwnershipEntry[] transfers;
+
+
+    function transferOwnership(address newOwner) public returns(bool success){
+        // Payment
+        if (msg.sender == owner){
+            owner = newOwner;
+            // transfers.push(TransferOwnershipEntry(msg.sender,owner)); 
+            emit TransferCompleted(msg.sender,newOwner,batchId);
+            return true;
+        }
+        return false;
     }
+    
+    ////////////////////////////////////////////////////////////////
+    
+    //Transfer location 
+    
+    address public locationOwner;
+    address public newLocationOwner;
+    bool public islocationbeingtransfered = false;
+    TransferLocationEntry[] locationTransfers;
+    
+    struct TransferLocationEntry{
+        address from;
+        address to;
+    }
+    
+    
+    function transferLocation(address _newLocationOwner) public returns(bool success){
+        if (msg.sender == locationOwner && !islocationbeingtransfered) {
+            newLocationOwner = _newLocationOwner;
+            islocationbeingtransfered = true;
+            return true;
+        }
+        return false;
+    }
+    
+    function approveTransferLocation(string[] memory _bagIds) public returns(bool success){
+        if (msg.sender ==  newLocationOwner && islocationbeingtransfered){
+            if (keccak256(abi.encode(bagIds)) == keccak256(abi.encode(_bagIds))) {
+                locationTransfers.push(TransferLocationEntry(locationOwner,newLocationOwner));
+                locationOwner = newLocationOwner;
+                newLocationOwner = address(0);
+                islocationbeingtransfered = false;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+//////////////////////////////////////////////////////////////////////    
+    
+    
+    struct TransferBagEntry{
+        address mod;
+        string[] previousBagIds;
+        string[] newBagIds;
+    }
+    
+    
+    
+
+    
+    
+    
+    
+    /////////////////////////////////////////////////////
+    
+
     
     
     
@@ -49,17 +156,9 @@ contract Coffee
     //     batchId = _cid;
     // }
     
-    function transferOwnership(address newOwner) public returns(bool success){
-        if (msg.sender == owner){
-            owner = newOwner;
-            transfers.push(TransferEntry(msg.sender,owner)); 
-            emit TransferCompleted(msg.sender,newOwner,batchId);
-            return true;
-        }
-        return false;
-    }
     
-    function getTransfers() public returns(TransferEntry[] memory allTransfers){
+    
+    function getTransfers() public returns(TransferOwnershipEntry[] memory allTransfers){
         return transfers;
     }
     
